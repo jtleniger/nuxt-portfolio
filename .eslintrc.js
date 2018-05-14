1e95403ecb3a78ee622c78cd987389a86c018c44
Martin Duenkelmann <martin.duenkelmann@smail.inf.h-brs.de>
2018-05-14 19:12:35 +0200

    Enforce the power of eslint.
    Now i need to fix the file syntax.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,31 @@
+/*
+ * Created by Martin Dünkelmann on 14.05.18 18:58
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 14.05.18 18:41
+ */
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true,
   },
-  extends: 'standard',
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
   rules: {},
   globals: {}
-}
+};
