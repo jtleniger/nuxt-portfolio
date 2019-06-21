209e720921ff2737baf81161412325f337654d4d
Justin Leniger <justin.leniger@gmail.com>
2019-06-21 00:10:26 -0600

    poem

---
+++ pages/thoughts/wewere.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <section class="hero is-dark is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <div class="column">
+            <h1 class="title is-size-2-mobile has-text-weight-bold has-text-right-mobile">We were.</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <div class="tile is-child box">
+              <p>
+                <span class="tag is-medium">June 20, 2019</span>
+              </p>
+                <p class="margin-top poem">
+                    We were.
+                </p>
+                <p class="margin-top poem">
+                    We were young and hopeful.
+                    We were courageous and passionate.
+                    Clever. Resourceful.
+                </p>
+                <p class="margin-top poem">
+                    We build great things.
+                    We discovered; live, and died.
+                    Explored. Invented.
+                </p>
+                <p class="margin-top poem">
+                    We loved.
+                </p>
+                <p class="margin-top poem">
+                    We were hateful and fearful.
+                    We exploited resources and extinguished species.
+                    Bloodthirsty. Power hungry.
+                </p>
+                <p class="margin-top poem">
+                    We destroyed and murdered.
+                    We polluted and poisoned.
+                    Shortsighted. Selfish.
+                </p>
+                <p class="margin-top poem">
+                    We wondered.
+                </p>
+                <p class="margin-top poem">
+                    We saw beauty.
+                    We were optimistic for the future.
+                    We wanted to understand it all.
+                </p>
+                <p class="margin-top poem">
+                    We were.
+                </p>
+                <p class="margin-top poem">
+                    But we weren’t the ones.
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style>
+.poem {
+  text-align: center;
+}
+</style>
