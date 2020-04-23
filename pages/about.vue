a8528a8969853bb238276e832ddc09dac16062f9
Justin Leniger <jtleniger@gmail.com>
2020-04-22 23:06:53 -0600

    about page

---
+++ pages/about.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="body">
+      <h1>I am...</h1>
+      <div class="cards">
+        <div>
+          <h1>A Software Developer</h1>
+          <p>
+            I write software both professionally and in my free time. This site is on my
+            <a
+              href="https://github.com/jtleniger"
+              target="_blank"
+            >GitHub.</a>
+          </p>
+        </div>
+        <div>
+          <h1>An Amateur Photographer</h1>
+          <p>
+            Pictures can share much more than a scene; they can share a feeling or tell a story. I love improving my skills in my freetime. Check out my
+            <a
+              href="https://unsplash.com/@jtleniger"
+              target="_blank"
+            >Unsplash.</a>
+          </p>
+        </div>
+        <div>
+          <h1>An Outdoorsman</h1>
+          <p>When I'm not at work I try to get into nature as much as possible.</p>
+        </div>
+      </div>
+    </div>
+</template>
+<style lang="scss">
+body h1 {
+  margin: 1rem 2rem;
+}
+</style>
