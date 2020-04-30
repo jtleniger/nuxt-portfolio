ce7987d37d5973e458b04e2195856a7b1182848b
Justin Leniger <jtleniger@gmail.com>
2020-04-29 21:42:32 -0600

    added thank you page

---
+++ pages/thank-you.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="content narrow">
+    <section class="title">
+      <h1>Thank you!</h1>
+      <p>I'll be in touch shortly.</p>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  created() {
+    setTimeout(() => this.$router.push({ path: '/' }), 3000)
+  }
+}
+</script>
