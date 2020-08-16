81435489f8ff2e05f4414f6fc581433beb8953f7
Justin Leniger <jtleniger@gmail.com>
2020-08-16 12:36:21 -0600

    lazy load

--- components/lazy-image.vue
+++ components/lazy-image.vue
@@ -2,12 +2,12 @@
   <picture>
     <source :srcSet="require('~/assets/images/' + src + '?webp')" type="image/webp" />
     <source :srcSet="require('~/assets/images/' + src)" type="image/jpeg" />
-    <img :src="require('~/assets/images/' + src)" />
+    <img :src="require('~/assets/images/' + src)" :alt="alt" loading="lazy"/>
   </picture>
 </template>
 
 <script>
 export default {
-  props: ['src']
+  props: ['src', 'alt']
 }
 </script>
